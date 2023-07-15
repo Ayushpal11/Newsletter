8b0ec8081ba444567d6225506786e61f551dad44
Ayushpal11 <ayushpal74553@gmail.com>
2023-07-15 06:34:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'Signup.html'));
+});
+
+app.post('/', function(req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: 'subscribed',
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us21.api.mailchimp.com/3.0/lists/695743e77b';
+    const options = {
+        method: 'POST',
+        auth: 'ayush:1827a96064eaed6e49e7ab38b244f08c-us21'
+    };
+
+    const request = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendFile(path.join(__dirname, 'success.html'));
+        } else {
+            res.sendFile(path.join(__dirname, 'failure.html'));
+        }
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post('/failure', function(req, res) {
+    res.redirect('/');
+});
+
+app.listen(4000, function() {
+    console.log('Server is running on port 4000');
+});
